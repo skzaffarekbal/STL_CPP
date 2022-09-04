02d16f1fbc5e9f8da3adc33d3ba0c1ebc8664af5
Sk Zaffar Ekbal <skzaffar.ekbal@nettantra.net>
2022-09-04 12:12:19 +0530

    Map concept

---
+++ 10_map.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<int, string> m;
+    m[1] = " abc "; // O(logN)
+    m[5] = " cdc ";
+    m[3] = " acd ";
+    m[3] = " xyz "; // Just update the key 3 value
+    m.insert({4, " afg "});
+    for (auto &pr : m) // Access Time complexity O(NlogN)
+    {
+        cout << pr.first << " " << pr.second << endl;
+    }
+
+    auto it = m.find(3); // O(logN)
+    if (it == m.end())
+    {
+        cout << "No Value" << endl;
+    }
+    else
+    {
+        cout << (*it).first << " " << (*it).second << endl;
+        m.erase(it); // to remove from map O(logN)
+    }
+
+    m[3] = " acd ";
+    m.erase(3);
+}
